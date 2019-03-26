53e645ff128a1d30d47bccea686eab864103ae75
CodeMode.io <codemode.io@gmail.com>
2019-03-26 14:53:40 -0600

    update database name to 'yelp_camp'

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index"),
     reviewRoutes = require("./routes/reviews");
 
-mongoose.connect('mongodb+srv://CodeMode:admin@yc-cluster-meeem.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://CodeMode:admin@yc-cluster-meeem.mongodb.net/yelp_camp?retryWrites=true', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
