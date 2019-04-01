6534914361355291a4cb2f93727bc74540863f19
CodeMode.io <codemode.io@gmail.com>
2019-04-01 10:57:17 -0600

    remove map on mobile, add forgot password link, move map stylesheet to .css stylesheet, fix route when logging into non-existent account.

--- routes/index.js
+++ routes/index.js
@@ -55,7 +55,7 @@ router.get("/login", function(req, res){
 router.post("/login", passport.authenticate("local",
     {
         successRedirect: "/campgrounds",
-        failureRedirect: "/login",
+        failureRedirect: "/register",
     }), function(req, res){
 });
 
